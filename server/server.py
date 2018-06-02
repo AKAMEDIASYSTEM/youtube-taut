e5ba9ac45f0b83b333fb330398e588ba2ded3bee
AKA <nfeehan@gmail.com>
2018-06-02 19:30:02 -0400

    adding server

--- server/server.py
+++ server/server.py
@@ -30,20 +30,6 @@ class BaseHandler(tornado.web.RequestHandler):
         self.finish()
 
 
-class MainHandler(tornado.web.RequestHandler):
-    """Make the app."""
-
-    def get(self):
-        """Make the app."""
-        self.set_header('Access-Control-Allow-Origin', '*')
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-        self.set_header('Access-Control-Max-Age', 1000)
-        # self.set_header('Access-Control-Allow-Headers', 'origin, x-csrftoken, content-type, accept')
-        self.set_header('Access-Control-Allow-Headers', '*')
-        self.set_header('Content-type', 'application/json')
-        self.write("Hello, world")
-
-
 def make_app():
     """Make the app."""
     return tornado.web.Application([
