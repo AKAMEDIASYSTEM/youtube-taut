108e0e17745ee9188670d1c0d5ac482e8bcd6942
AKA <nfeehan@gmail.com>
2018-06-02 19:26:26 -0400

    adding server

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ class BaseHandler(tornado.web.RequestHandler):
         """Make the app."""
         print "setting headers!!!"
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
 
     def post(self):
