64c9874de58419a1735cb6a48330c79e34cedda3
AKA <nfeehan@gmail.com>
2018-06-02 18:46:12 -0400

    adding server

---
+++ server/server.py
@@ -0,0 +1,25 @@
+"""Make the app."""
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    """Make the app."""
+
+    def get(self):
+        """Make the app."""
+        self.write("Hello, world")
+
+
+def make_app():
+    """Make the app."""
+    return tornado.web.Application([
+        (r"/taut", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    """Make the app."""
+    app = make_app()
+    app.listen(80)
+    tornado.ioloop.IOLoop.current().start()
