bdd6935fde50c96a1643d3242637166783ad7fc3
AKA <nfeehan@gmail.com>
2018-06-02 21:12:04 -0400

    server tweaks

--- server/server.py
+++ server/server.py
@@ -2,6 +2,9 @@
 import tornado.ioloop
 import tornado.web
 import json
+import logging
+from tornado.log import enable_pretty_logging
+enable_pretty_logging()
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -9,7 +12,8 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def set_default_headers(self):
         """Make the app."""
-        print("setting headers, does this happen often")
+        # print("setting headers, does this happen often")
+        logging.debug("setting headers, does this happen often")
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
@@ -18,14 +22,16 @@ class BaseHandler(tornado.web.RequestHandler):
         """Make the app."""
         payload = json.loads(self.request.body)
         for i in payload:
-            ss = " : ".join([str(i), str(payload[i])])
-            print(ss)
-            # print(payload[i])
+            ss = ": ".join([str(i), str(payload[i])])
+            # print(ss)
+            logging.debug(ss)
+            print(payload[i])
+            logging.debug(payload[i])
         self.write('cool youtube action buddy')
 
     def get(self):
         """Make the app."""
-        self.write('some get')
+        self.write("My dude, you're not supposed to do GETs on this server")
 
     def options(self):
         """Make the app."""
@@ -44,6 +50,7 @@ def make_app():
 if __name__ == "__main__":
     """Make the app."""
     app = make_app()
-    print("listening now")
+    # print("listening now")
+    logging.debug("listening now")
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
