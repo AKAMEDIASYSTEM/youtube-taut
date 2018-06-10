6307cc9dccffa26aa781a975fc78f0b2fef8157f
AKA <nfeehan@gmail.com>
2018-06-10 18:51:06 -0400

    resuming work here

--- server/server.py
+++ server/server.py
@@ -7,7 +7,8 @@ import logging
 from tornado.log import enable_pretty_logging
 
 enable_pretty_logging()
-logging.basicConfig(filename='/var/www/youtube-taut/youtube-taut-server.log', level=logging.DEBUG)
+# logging.basicConfig(filename='/var/www/youtube-taut/youtube-taut-server.log', level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 # settings = {'debug': True, 'auth': True}
 settings = {'debug': True}
 
